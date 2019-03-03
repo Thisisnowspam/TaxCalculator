a2060e79183b2c6a6428dba77e31a5fd7957be2e
irondextros <48073235+irondextros@users.noreply.github.com>
2019-03-02 22:27:06 -0600

    Testing Commit

--- Tax calculator.py
+++ Tax calculator.py
@@ -10,32 +10,27 @@ while True:
     print ("Made by Joshua Hillis")
     time.sleep(.5)
 
+    tax = float(input("What is the tax rate we must apply to the item?"))
+    price = float(input("What's the price of the item?"))
 
-
-
-    tax = input("What is the tax rate we must apply to the item?")
-    price = input("What's the price of the item?")
-    
+    if tax > 1:
+        tax = (tax / 100)
 
 
     if re.search('[^\d\.$]', tax) or re.search('[^\d\.$%]', price):
         print ("please enter valid response")
         time.sleep(1)
     else:
-        continue
+        re.sub(('[^\d\.]', '', str(tax))
+        re.sub(('[^\d\.]', '', str(price))
 
+        if tax > (100):
+            tax = (tax / 100)
 
-    price = float(input("What's the price of the item?"))
-    tax = float(input("What is the tax rate we must apply to the item?"))
-    if tax > 1:
-        (tax/100)
-        
+        total = price + (price * tax)
+        total = round(total, 2)
 
-    total = price + (price * tax)
-    total = round(total, 2)
-
-
-    print(total)
+        print(total)
     while True:
         answer = input("Repeat? (y/n:)")
         if answer in ("y","n").lower():
