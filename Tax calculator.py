c418ebd10fb493a46abd479b64ef7423f2358f07
irondextros <48073235+irondextros@users.noreply.github.com>
2019-03-02 21:36:11 -0600

    Initial commit

---
+++ Tax calculator.py
@@ -0,0 +1,50 @@
+
+import os
+import re
+from decimal import Decimal
+import time
+while True:
+
+    def inputFromUser():
+        global tax
+        global price
+
+    print ("Made by Joshua Hillis")
+    time.sleep(.5)
+
+
+
+
+    tax = input("What is the tax rate we must apply to the item?")
+    price = input("What's the price of the item?")
+    
+
+
+    if re.search('[^\d\.$]', tax) or re.search('[^\d\.$%]', price):
+        print ("please enter valid response")
+        time.sleep(1)
+    else:
+        continue
+
+
+    price = float(input("What's the price of the item?"))
+    tax = float(input("What is the tax rate we must apply to the item?"))
+    if tax > 1:
+        (tax/100)
+        
+
+    total = price + (price * tax)
+    total = round(total, 2)
+
+
+    print(total)
+    while True:
+        answer = input("Repeat? (y/n:)")
+        if answer in ("y","n").lower():
+            break
+        print ("trying to break my program, bro?")
+        if answer == "y":
+            continue
+        else:
+            print ("Goodbye.")
+            break
