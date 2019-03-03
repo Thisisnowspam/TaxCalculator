c9bcdb6041de3cd56163737a14ace326b7a31474
irondextros <48073235+irondextros@users.noreply.github.com>
2019-03-02 22:49:47 -0600

    Testing Commit

--- Tax calculator.py
+++ Tax calculator.py
@@ -3,9 +3,8 @@ import re
 import time
 while True:
 
-    def inputFromUser():
-        global tax
-        global price
+    global tax
+    global price
 
     print ("Made by Joshua Hillis")
     time.sleep(.5)
@@ -16,13 +15,12 @@ while True:
     if tax > 1:
         tax = (tax / 100)
 
-
-    if re.search('[^\d\.$]', tax) or re.search('[^\d\.$%]', price):
+    if re.search('[^\d\.$]', (tax)) or re.search('[^\d\.$%]', (price)):
         print ("please enter valid response")
         time.sleep(1)
     else:
-        re.sub(('[^\d\.]', '', str(tax))
-        re.sub(('[^\d\.]', '', str(price))
+        re.sub('[^\d\.]', '', str(tax))
+        re.sub('[^\d\.]', '', str(price))
 
         if tax > (100):
             tax = (tax / 100)
@@ -32,12 +30,22 @@ while True:
 
         print(total)
     while True:
-        answer = input("Repeat? (y/n:)")
-        if answer in ("y","n").lower():
-            break
-        print ("trying to break my program, bro?")
-        if answer == "y":
-            continue
+        answer = input("Repeat? (y/n:)").lower()
+
+        if answer == "y" or answer == "n":
+            if answer == "y":
+                tax = float(input("What is the tax rate we must apply to the item?"))
+                price = float(input("What's the price of the item?"))
+
+                if tax > 1:
+                    tax = (tax / 100)
+            if re.search('[^\d\.$]', (tax)) or re.search('[^\d\.$%]', (price)):
+                print("please enter valid response")
+                time.sleep(1)
+            else:
+                re.sub('[^\d\.]', '', str(tax))
+                re.sub('[^\d\.]', '', str(price))
+
         else:
             print ("Goodbye.")
             break
